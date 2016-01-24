5c6e64c52625fe36de81f3b9b64adcce3cb97af7
Michael Young <mike@bitready.io>
2016-01-23 22:04:57 -0500

    Delete links.js

--- app/static/js/links.js
+++
@@ -1,3 +0,0 @@
-function facebook()	{	window.location="https://facebook.com/UnbiasedPotProject";	}
-function twitter()	{	window.location="https://twitter.com/#!/UnbiasedPotProj";	}
-function wordpress(){	window.location="http://unbiasedpotproject.org/news/";		}
